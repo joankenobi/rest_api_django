ab5dcf7dcaea0217434367ddce4574ef780cbaa4
joankenobi <joan1233blanco@gmail.com>
2024-03-11 17:21:00 -0400

    show object model with its tittle

--- tasks/models.py
+++ tasks/models.py
@@ -5,3 +5,7 @@ class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
     completed = models.BooleanField(default=False, blank=True, null=True)
+
+    def __str__(self):
+        return self.title # al instanciar el modelo, se mostrara el titulo del modelo.
+    
