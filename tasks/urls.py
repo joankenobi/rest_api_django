0145e91bdd627bca7223bf290eb9bb0303735a84
joankenobi <joan1233blanco@gmail.com>
2024-03-13 21:54:27 -0400

    feat: agregated the routes

---
+++ tasks/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework import routers
+from tasks.views import TaskViewSet
+
+# generate all the urls for the API (GET, POST, PUT, DELETE) in tasks model
+
+router = routers.DefaultRouter()
+router.register(r"tasks", TaskViewSet, basename="tasks")
+
+urlpatterns = [
+    path("api/v1" , include(router.urls))
+]
+
